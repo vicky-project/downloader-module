e62bc77ad9cd03630436871d71bb676ec6c36656
vicky <pratamavr@gmail.com>
2025-12-05 10:56:49 +0800

    update menu

--- app/Providers/MenuProvider.php
+++ app/Providers/MenuProvider.php
@@ -2,7 +2,7 @@
 namespace Modules\Downloader\Providers;
 
 use Modules\MenuManagement\Interfaces\MenuProviderInterface;
-use Modules\Financial\Constants\Permission;
+use Modules\Downloader\Constants\Permission;
 
 class MenuProvider implements MenuProviderInterface
 {
