703ecaa5404d4b58e743aafd634fcc9e081c9bf1
vicky <pratamavr@gmail.com>
2025-12-05 11:53:02 +0800

    update menu

--- app/Constants/Permissions.php
+++ app/Constants/Permissions.php
@@ -4,9 +4,13 @@ namespace Modules\Downloader\Constants;
 class Permissions
 {
 	const VIEW_DOWNLOADERS = "downloader.view";
+	const MANAGE_DOWNLOADS = "downloader.manage";
 
 	public static function all(): array
 	{
-		return [self::VIEW_DOWNLOADERS => "View Downloader"];
+		return [
+			self::VIEW_DOWNLOADERS => "View Downloader",
+			self::MANAGE_DOWNLOADS => "Manage Downloader",
+		];
 	}
 }
